2812dc4e9e19f1282df2b645eabf9f8e38d24ea8
minolmal <minolmalshan01@gmail.com>
2023-01-11 22:58:31 +0530

    cors update 2

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://abc-institute.onrender.com",
   "http://localhost:3000",
   "https://abc-server-minolmal.vercel.app/",
+  "https://final-assignment-lemon.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
