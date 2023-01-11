ed4ef1fa57df50d238b4a3e42c72d06c9b3f8e5e
minolmal <minolmalshan01@gmail.com>
2023-01-11 22:17:32 +0530

    vercel added

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://abc-institute.onrender.com",
   "http://localhost:3000",
+  "https://final-assignment-lemon.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
